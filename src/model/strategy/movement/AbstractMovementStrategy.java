b6205d3d84d2bed2047fc7ab86425b5759a57f25
Thibault Boksebeld <thibault.boksebeld@mastertp>
2015-11-06 14:33:39 +0100

    finish

--- src/model/strategy/movement/AbstractMovementStrategy.java
+++ src/model/strategy/movement/AbstractMovementStrategy.java
@@ -1,40 +1,41 @@
 package model.strategy.movement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import model.Couleur;
 
 public abstract class AbstractMovementStrategy {
 
 	private static int mode = 0;
+	private static Map<Integer, String> mapRegleTempete = new HashMap<Integer, String>() {
+		{
+			put(0, "Tour");
+			put(7, "Tour");
+			put(1, "Cavalier");
+			put(6, "Cavalier");
+			put(2, "Fou");
+			put(5, "Fou");
+		}
+	};
 
 	public static void setMode(int mode) {
 		AbstractMovementStrategy.mode = mode;
 	}
 
 	public static AbstractMovementStrategy getNewStrategy(String type, int x) {
-		//System.out.println("AbstractMovementStrategy - type = " + type + " - x = " + x);
+		// System.out.println("AbstractMovementStrategy - type = " + type +
+		// " - x = " + x);
 		if (AbstractMovementStrategy.mode == 1) {
-			//System.out.println("AbstractMovementStrategy.mode == 1");
-			if (type.equals("Tour") || type.equals("Fou") || type.equals("Cavalier")) {
-				switch (x) {
-				case 0:
-				case 7:
-					//System.out.println("Devient une tour batard!");
-					return AbstractMovementStrategyFactory
-							.createMovementStrategy("Tour");
-				case 1:
-				case 6:
-					//System.out.println("Devient un cavalier batard!");
-					return AbstractMovementStrategyFactory
-							.createMovementStrategy("Cavalier");
-				case 2:
-				case 5:
-					//System.out.println("Devient un fou batard!");
-					return AbstractMovementStrategyFactory
-							.createMovementStrategy("Fou");
-				default:
-					//System.out.println("Redevient normal batard!");
+			// System.out.println("AbstractMovementStrategy.mode == 1");
+			if (type.equals("Tour") || type.equals("Fou")
+					|| type.equals("Cavalier")) {
+				if (!mapRegleTempete.containsKey(x)) {
 					return AbstractMovementStrategyFactory
 							.createMovementStrategy(type);
+				} else {
+					return AbstractMovementStrategyFactory
+							.createMovementStrategy(mapRegleTempete.get(x));
 				}
 			}
 		}
